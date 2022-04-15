daf8d7f4935dcb6b34ea04085c621ab7053f93cd
wwb <363187257@qq.com>
2022-04-15 20:19:19 +0800

    my first commit

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -2,5 +2,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("xufennian");
         System.out.println("xuanyuanqinfen");
+        System.out.println("laohuang");
     }
 }
